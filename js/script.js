5799e6246ad8e835600fad388e8be12dcc9fc222
SteveQ19 <ksteveqh@gmail.com>
2023-05-11 22:27:44 -0500

    first version

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// header
+
+let menuBtn = document.getElementById('menuBtn');
+
+menuBtn.addEventListener('click', function(e) {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark');
+});
+
+// typing effect
+
+let typed = new Typed('.auto-input', {
+    strings:['Geological Engineer','Data Analytics','Front-End Developer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+});
+
+// active link state on scroll
+
+// get all links
+let navlinks = document.querySelectorAll('nav ul li a')
+
+// get all section
+
+let sections = document.querySelectorAll('section')
+
+
+window.addEventListener('scroll', function(){
+    const scrollPos = window.scrollY + 20
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+                if (section.getAttribute('id') === link.getAttribute('href').substring(1)) {
+                    link.classList.add('active')
+                }
+            });
+        }
+    });
+})
